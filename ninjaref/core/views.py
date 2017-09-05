c37638cfd895063f5a462ab0bc365bd749f2dccb
Joseph Kato <joseph@jdkato.io>
2017-09-04 22:01:17 -0700

    fix: update navbar classes

--- ninjaref/core/views.py
+++ ninjaref/core/views.py
@@ -1,23 +1,24 @@
 from django.shortcuts import render
 
-from .models import (
-    CareerSummary,
-    Ninja
-)
+from .models import (CareerSummary, Ninja)
 
 
 def index(request):
     ninjas = Ninja.objects.all()
     names = ['{0} {1}'.format(n.first_name, n.last_name) for n in ninjas]
-    print(names)
-    return render(request, 'core/index.html', {'names': names})
+    return render(request, 'core/index.html',
+                  {'names': names,
+                   'page': 'index'})
 
 
 def leaderboard(request):
     r = {'rating': 'speed + consistency + success'}
     leaders = CareerSummary.objects.all().extra(select=r).order_by('-rating')
-    return render(request, 'core/leaderboard.html', {
-        'leaders': leaders[:10],
-        'men': leaders.filter(ninja__sex='M')[:10],
-        'women': leaders.filter(ninja__sex='F')[:10]
-    })
+    return render(
+        request,
+        'core/leaderboard.html', {
+            'leaders': leaders[:10],
+            'men': leaders.filter(ninja__sex='M')[:10],
+            'women': leaders.filter(ninja__sex='F')[:10],
+            'page': 'leaderboard'
+        })
